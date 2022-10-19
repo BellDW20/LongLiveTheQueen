f7b56ea45cd5b5eea6446c9507e1a3ad44ebe1ee
tpstjn <63107315+tpstjn@users.noreply.github.com>
2022-10-19 19:15:10 -0400

    Added dash to commando

--- Assets/Scripts/CommandoScript.cs
+++ Assets/Scripts/CommandoScript.cs
@@ -9,6 +9,10 @@ public class CommandoScript : MonoBehaviour
     Transform _transform;
 
     float _movementSpeed = 5;
+    const float DASH_SPEED = 10;
+    const float DASH_TIME = 0.1f;
+    bool _isDashing = false;
+    float _dashStart;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +24,18 @@ public class CommandoScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _rbody.velocity *= DASH_SPEED;
+            _isDashing = true;
+            _dashStart = Time.time;
+        }
+
+        if (Time.time - _dashStart >= DASH_TIME)
+        {
+            _rbody.velocity = (new Vector2(_rbody.velocity.normalized.x, _rbody.velocity.normalized.y) * _movementSpeed); ;
+            _isDashing = false;
+        }
         if (_rbody.velocity != Vector2.zero)
         {
             _transform.up = _rbody.velocity;
@@ -28,6 +44,8 @@ public class CommandoScript : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (_isDashing) return;
+
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
