822dbeb0dbe1a90d6db2af312dd689c0b17fbf8f
L23-GCC <63116121+L23-GCC@users.noreply.github.com>
2022-10-19 19:48:59 -0400

    sniper shooting

--- Assets/Scripts/SniperScript.cs
+++ Assets/Scripts/SniperScript.cs
@@ -8,7 +8,17 @@ public class SniperScript : MonoBehaviour
     SpriteRenderer _sRender;
     Transform _transform;
 
+    public GameObject _bullet;
+
+    Vector3 _currentDirection;
     float _movementSpeed = 5;
+    float _fireDelay = 0.5f;
+    float _shotVelocity = 10;
+    float _shotTimer = 0.0f;
+    int _magSize = 6;
+
+    float _maxLife = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,9 +30,24 @@ public class SniperScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        _currentDirection = _rbody.velocity.normalized;
         if (_rbody.velocity != Vector2.zero)
         {
             _transform.up = _rbody.velocity;
+        }
+
+        if (Input.GetMouseButton(0) && Time.time - _shotTimer > _fireDelay)
+        {
+            Vector3 tempDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            GameObject tempBullet = Instantiate(_bullet, _transform.position, Quaternion.identity);
+
+            Vector3 shotDirection = new Vector3(tempDirection.x - _transform.position.x, tempDirection.y - _transform.position.y, 0);
+
+            _transform.up = shotDirection;
+            tempBullet.transform.up = shotDirection;
+
+            tempBullet.GetComponent<Rigidbody2D>().velocity = shotDirection.normalized * _shotVelocity;
+            _shotTimer = Time.time;
         }
     }
 
