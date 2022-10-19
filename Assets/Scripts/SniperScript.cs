a12ab0b6440d126e1ccaeedec9fb7ffdfdfd005b
L23-GCC <63116121+L23-GCC@users.noreply.github.com>
2022-10-19 19:58:59 -0400

    reload for sniper

--- Assets/Scripts/SniperScript.cs
+++ Assets/Scripts/SniperScript.cs
@@ -12,10 +12,19 @@ public class SniperScript : MonoBehaviour
 
     Vector3 _currentDirection;
     float _movementSpeed = 5;
+    const float DASH_SPEED = 10;
+    const float DASH_TIME = 0.1f;
+    bool _isDashing = false;
+    float _dashStart;
+
     float _fireDelay = 0.5f;
     float _shotVelocity = 10;
     float _shotTimer = 0.0f;
+
     int _magSize = 6;
+    int _bulletsInMag = 6;
+    float _reloadTimer = 0.0f;
+    float _reloadDelay = 1;
 
     float _maxLife = 10;
 
@@ -36,7 +45,30 @@ public class SniperScript : MonoBehaviour
             _transform.up = _rbody.velocity;
         }
 
-        if (Input.GetMouseButton(0) && Time.time - _shotTimer > _fireDelay)
+        if (Time.time - _reloadTimer > _reloadDelay && _reloadTimer != 0)
+        {
+            _reloadTimer = 0.0f;
+            _bulletsInMag = 6;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _rbody.velocity *= DASH_SPEED;
+            _isDashing = true;
+            _dashStart = Time.time;
+        }
+
+        if (Time.time - _dashStart >= DASH_TIME)
+        {
+            _rbody.velocity = (new Vector2(_rbody.velocity.normalized.x, _rbody.velocity.normalized.y) * _movementSpeed); ;
+            _isDashing = false;
+        }
+        if (_rbody.velocity != Vector2.zero)
+        {
+            _transform.up = _rbody.velocity;
+        }
+
+        if (Input.GetMouseButton(0) && Time.time - _shotTimer > _fireDelay && _bulletsInMag > 0)
         {
             Vector3 tempDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             GameObject tempBullet = Instantiate(_bullet, _transform.position, Quaternion.identity);
@@ -48,11 +80,20 @@ public class SniperScript : MonoBehaviour
 
             tempBullet.GetComponent<Rigidbody2D>().velocity = shotDirection.normalized * _shotVelocity;
             _shotTimer = Time.time;
+
+            _bulletsInMag--;
+
+            if (_bulletsInMag == 0)
+            {
+                _reloadTimer = Time.time;
+            }
         }
     }
 
     private void FixedUpdate()
     {
+        if (_isDashing) return;
+
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
 
