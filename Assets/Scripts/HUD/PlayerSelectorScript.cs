ba7cd958ea054413803275c1b2500979a4d2cc22
BELLDW20 <BELLDW20@gcc.edu>
2022-12-08 10:30:33 -0500

    Updated menus to be a bit nicer

--- Assets/Scripts/HUD/PlayerSelectorScript.cs
+++ Assets/Scripts/HUD/PlayerSelectorScript.cs
@@ -8,10 +8,23 @@ public class PlayerSelectorScript : MonoBehaviour {
 
     [SerializeField] private Text _playerText;
     [SerializeField] private Text _selectionText;
+    [SerializeField] private Image _characterImage;
+    [SerializeField] private Text _characterWeaponText;
+    [SerializeField] private Text _characterSpecialText;
+
+    [SerializeField] private Sprite[] CHARACTER_IMAGES;
+    private static string[] CHARACTER_WEAPON_NAMES = new string[] { "Assault Rifle", "Sniper Rifle", "Flamethrower" };
+    private static string[] CHARACTER_SPECIAL_NAMES = new string[] { "Molotov Cocktail", "Piercing Shot", "Flame Dash" };
+
     public int _joystickNumber;
     public int _selection;
 
     private bool _movedLast;
+
+    private void Start() {
+        UpdateSelection();
+    }
+
     void Update() {
         float xInput = InputManager.GetHorizontalInput(_joystickNumber);
         if (xInput < -0.5f && !_movedLast) {
@@ -25,9 +38,12 @@ public class PlayerSelectorScript : MonoBehaviour {
     }
 
     private void UpdateSelection() {
-        if(_selection < 0) { _selection = 3; }
-        else if (_selection >= 4) { _selection = 0; }
+        if(_selection < 0) { _selection = 2; }
+        else if (_selection >= 3) { _selection = 0; }
         _selectionText.text = PlayerInfo.PLAYER_NAME[_selection];
+        _characterImage.sprite = CHARACTER_IMAGES[_selection];
+        _characterWeaponText.text = "WEAPON: "+CHARACTER_WEAPON_NAMES[_selection];
+        _characterSpecialText.text = "SPECIAL: "+CHARACTER_SPECIAL_NAMES[_selection];
     }
 
     public void Init(int playerNumber) {
