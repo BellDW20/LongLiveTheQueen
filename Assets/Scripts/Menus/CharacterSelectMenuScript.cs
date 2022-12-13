7f5c1be72bcc482c9283695f7e97da7b40ec6d29
BELLDW20 <BELLDW20@gcc.edu>
2022-12-12 21:53:55 -0500

    Why do you do this git

--- Assets/Scripts/Menus/CharacterSelectMenuScript.cs
+++ Assets/Scripts/Menus/CharacterSelectMenuScript.cs
@@ -49,7 +49,7 @@ public class CharacterSelectMenuScript : MonoBehaviour {
 
         //Then tell the level manager to start the first level using a full level transition
         if (LevelManagerScript.GetMode() == GameMode.ARCADE_MODE) {
-            LevelManagerScript.BeginLevel(Level.LEVEL_3, LevelManagerScript.LEVEL_TRANSITION);
+            LevelManagerScript.BeginLevel(Level.LEVEL_1, LevelManagerScript.LEVEL_TRANSITION);
         } else {
             LevelManagerScript.BeginLevel(Level.HORDE_MODE, LevelManagerScript.LEVEL_TRANSITION);
         }
